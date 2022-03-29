5dcdd85d84605279abba4a738eb53fad7b11b311
M-kirichenko <m.kirichenko.exceedteam@gmail.com>
2022-03-29 15:09:24 +0400

    final fix

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 const port = 3000;
+const db = require("./models");
 
 
 app.use(cors());
 app.use(express.json());
 
 require('./routes')(app);
+
+
+db.sequelize.sync();
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
